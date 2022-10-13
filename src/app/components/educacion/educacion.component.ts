2273df9f346ac04771a6491d3b6ae719592a6867
Gabu916 <csg.-@hotmail.com>
2022-10-13 20:05:50 -0300

    FrontEnd

---
+++ src/app/components/educacion/educacion.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Educacion } from 'src/app/model/educacion';
+import { EducacionService } from 'src/app/service/educacion.service';
+import { TokenService } from 'src/app/service/token.service';
+
+@Component({
+  selector: 'app-educacion',
+  templateUrl: './educacion.component.html',
+  styleUrls: ['./educacion.component.css']
+})
+export class EducacionComponent implements OnInit {
+  educacion: Educacion[] = [];
+
+  constructor(private educacionS: EducacionService, private tokenService: TokenService) { }
+  isLogged = false;
+
+  ngOnInit(): void {
+    this.cargarEducacion();
+    if (this.tokenService.getToken()) {
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+  }
+
+  cargarEducacion(): void {
+    this.educacionS.lista().subscribe(
+      data => {
+        this.educacion = data;
+      }
+    )
+  }
+
+  delete(id?: number) {
+    if (id != undefined) {
+      this.educacionS.delete(id).subscribe(
+        data => {
+          this.cargarEducacion();
+        }, err => {
+          alert("No se pudo eliminar");
+        }
+      )
+    }
+  }
+}
